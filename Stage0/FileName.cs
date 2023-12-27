207eeadc00e2eb9e9a9fc36d8db7e8bc481535e4
Meir1234! <meirke1999@gmail.com>
2023-12-27 22:08:40 +0200

    initial code + welcome function

---
+++ Stage0/FileName.cs
@@ -0,0 +1,27 @@
+﻿
+//namespace Ex1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            while (getAge() == 0) ;
+//        }
+//        static int getAge()
+//        {
+//            try
+//            {
+//                Console.WriteLine("enter your age");
+//                string ageStr = Console.ReadLine(); //48
+//                int age = int.Parse(ageStr);
+//                return age;
+//            }
+//            catch
+//            {
+//                Console.WriteLine("Please enter only numbers");
+//            }
+//            Console.WriteLine("after catch Block I'm still working");
+//            return 0;
+//        }
+//    }
+//}
---
+++ Stage0/Program8635.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8635();
+            Welcome1878();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome1878();
+        private static void Welcome8635()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
