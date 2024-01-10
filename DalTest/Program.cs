3e9add39676b08bc2d5537edfa891846a1579686
Meir1234! <meirke1999@gmail.com>
2024-01-10 20:11:14 +0200

    .

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,21 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
+namespace DalTest;
+
 using Dal;
 using DalApi;
-using DalFacade.DalApi;
-using DalFacade.DO;
-using DalTest;
 using DO;
 
 
 
-class Program
-
+static class Program
 {
     private static IEngineer? s_dalEngineer = new EngineerImplementation(); //stage 1
     private static IDependency? s_dalDependency = new DependencyImplementation(); //stage 1
     private static ITask? s_dalTask = new TaskImplementation(); //stage 1
-    Initialization.Do(s_dalEngineer, s_dalDependency, s_dalTask);
+    static void Main(string[] args)
+    {
+        Initialization.Do(s_dalEngineer, s_dalTask, s_dalDependency);
+        static void mainMnue()
+        {
+            int choice;
+            Console.WriteLine("Enter number to choose one of the following:\n" +
+                 "0:Exit\n" +
+                 "1:Engineer\n" +
+                 "2:Task\n" +
+                 "3:Dependency\n");
 
+            choice = Console.Read();
+
+            switch (choice)
+            {
+
+                case 0:
+                    return;
+                case 1:
+
+            }
+
+        }
+      
+    }
 }
 
 
