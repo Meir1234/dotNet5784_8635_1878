47c60a358c4d5eb61d022756bc028b76428081c5
sraya1 <prager@neto.net.il>
2024-01-18 21:00:49 +0200

    a

--- DalXml/TaskImplementation.cs
+++ DalXml/TaskImplementation.cs
@@ -2,35 +2,65 @@
 namespace Dal;
 using DalApi;
 using DO;
+using System.Net;
+
 internal class TaskImplementation : ITask
 {
+    readonly string s_tasks_xml = "tasks";
     public int Create(Task item)
     {
-        throw new NotImplementedException();
+        List<Task> Tasks = XMLTools.LoadListFromXMLElement<Task>(s_tasks_xml);
+        int nextId = Config.NextTaskId;
+        Task copy= item with(IDal = nextId);
+        Task.Add(copy);
+        XMLTools.SaveListToXMLElement(Tasks, s_tasks_xml);
     }
+
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        
     }
 
     public Task? Read(int id)
     {
-        throw new NotImplementedException();
+        
     }
 
     public Task? Read(Func<Task, bool> filter)
     {
-        throw new NotImplementedException();
+        
     }
 
     public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
     {
-        throw new NotImplementedException();
+       
     }
 
     public void Update(Task item)
     {
-        throw new NotImplementedException();
+        List<Task> Tasks = XMLTools.LoadListFromXMLElement<Task>(s_tasks_xml);
+        {
+            {
+                int updatedObjectId = item.Id;
+                bool found = false;
+
+                foreach (Task? obj in DataSource.Tasks)
+                {
+                    if (obj.Id == updatedObjectId)
+                    {
+                        DataSource.Tasks.Remove(obj);
+                        DataSource.Tasks.Add(item);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    throw new ArgumentException($"Task with ID {updatedObjectId} does not exist.");
+                }
+            }
+
+        }
     }
-}
