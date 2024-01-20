bea9b5d10967d6a79813dd3aefbe6a2ec135d1a3
sraya1 <prager@neto.net.il>
2024-01-21 00:50:46 +0200

    CONSTRUCTOR

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -9,5 +9,18 @@ namespace DO
         string? Email,
         Enum? Level,
         double? Cost
-    );
+    )
+    {
+    // בנאי ריק עם אתחול פרמטרים ברירת מחדל
+    public Engineer()
+        : this(
+              Id: 0,
+              Name: null,
+              Email: null,
+              Level: null,
+              Cost: null
+          )
+    {
+    }
+}
  }
