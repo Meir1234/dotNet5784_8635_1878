fcbde5192b34347ac0c599a9e85876a7b61ea75d
sraya1 <prager@neto.net.il>
2024-01-25 20:48:33 +0200

    a

--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,12 +1,15 @@
 ﻿
 
 using DalApi;
+using System.Diagnostics;
 namespace Dal;
 
 //stage 3
-sealed public class DalXml : IDal
+sealed internal class DalXml : IDal
 {
-     public IDependency Dependency => new DependencyImplementation();
+    public static IDal Instance { get; } = new DalXml();
+    private DalXml() { }
+    public IDependency Dependency => new DependencyImplementation();
     public IEngineer Engineer => new EngineerImplementation();
     public ITask Task => new TaskImplementation();
 
