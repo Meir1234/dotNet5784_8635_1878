62c77b340ac493c0375e9813b0da0d0a5d01ed88
sraya1 <prager@neto.net.il>
2024-03-05 14:41:27 +0200

    bo

---
+++ BL/BO/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+
+public class Task
+{
+public int id
+        Id int
+  string Alias
+  string Description
+  datetime CreatedAtDate
+ TimeSpan RequiredEffortTime 
+  bool IsMilestone
+   DO.EngineerExperience Copmlexity
+  datetime StartDate
+  datetime ScheduledDate
+}
