86311f2db9f1082ec9a1838b821a1fc91db30891
sraya1 <prager@neto.net.il>
2024-03-06 12:51:24 +0200

    taskupdate

--- BL/BlApi/ITaskInList.cs
+++ BL/BlApi/ITaskInList.cs
@@ -8,4 +8,21 @@ namespace BlApi;
 
 internal interface ITaskInList
 {
+    public List<Task> ReadAll();
+    public Task? Build(int id);
+
+    public int Add(BO.Task item);
+
+    public void Update(BO.Task item);
+
+    public void Delete(int id);
+
+
+    public int Create(BO.Student item);
+    public BO.Student? Read(int id);
+    public IEnumerable<BO.StudentInList> ReadAll();
+    public void Update(BO.Student item);
+    public void Delete(int id);
+    public BO.StudentInCourse GetDetailedCourseForStudent(int StudentId, int CourseId);
+
 }
