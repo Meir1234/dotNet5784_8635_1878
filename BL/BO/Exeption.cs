6c1d567a09c6c8462bff208882b898c060ebbb1a
meirk <meirk@MEIRKE_WELNER>
2024-03-14 17:06:50 +0200

    TaskInLIst Implementation

---
+++ BL/BO/Exeption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+[Serializable]
+
+public class BlDoesNotExistException : Exception
+{
+    public BlDoesNotExistException(string? message) : base(message) { }
+    public BlDoesNotExistException(string message, Exception innerException)
+    : base(message, innerException) { }
+}
+
+public class BlAlreadyExistsException : Exception
+{
+    public BlAlreadyExistsException(string? message) : base(message) { }
+    public BlAlreadyExistsException(string message, Exception innerException)
+    : base(message, innerException) { }
+}
---
+++ BL/BlImplementation/ITaskInListImplementation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlImplementation;
+using BlApi;
+using DalApi;
+
+internal class ITaskInListImplementation : ITaskInList
+{
+    private DalApi.IDal _dal = DalApi.Factory.Get;
+    public IEnumerable<BO.TaskInList> ReadAll()
+    {
+        return (from DO.Task doTask in _dal.Task.ReadAll()
+                select new BO.TaskInList
+                {
+                    Id = doTask.Id,
+                    Alias = doTask.Alias,
+                    Level = (BO.Level)doTask.Hardness
+                });
+    }
+}
